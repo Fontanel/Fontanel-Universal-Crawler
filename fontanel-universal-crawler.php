2f15ef0f5857e76178c814740bcd2d05905b7f4e
Jasper Kennis <jasper@fontanel.nl>
2013-05-02 17:15:27 +0200

    Including settings file before initializing main class.

--- fontanel-universal-crawler.php
+++ fontanel-universal-crawler.php
@@ -13,6 +13,10 @@
 	@ini_set( 'display_errors', 'On' );
 	
 	// Import settings:
+	if( file_exists( dirname(__FILE__) . '/settings.php' ) ) {
+		require_once( dirname(__FILE__) . '/settings.php' );
+	}
+	
 	if( file_exists( dirname(__FILE__) . '/fontanel-universal-crawler.class.php' ) ) {
 		require_once( dirname(__FILE__) . '/fontanel-universal-crawler.class.php' );
 	}
