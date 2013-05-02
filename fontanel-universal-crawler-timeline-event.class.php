7e8dc5d0eddaa046d8df86bd96ac2ab0963dcc66
Jasper Kennis <jasper@fontanel.nl>
2013-05-02 15:55:41 +0200

    Issue in the events fixed, reference to file slug was old.

--- fontanel-universal-crawler-timeline-event.class.php
+++ fontanel-universal-crawler-timeline-event.class.php
@@ -7,18 +7,18 @@
 			
 			public function __construct( $objects, $template_path = false ) {
 				$this->objects = $objects;
-				if( !$template ) {
-  				$this->findTempate();
+				if( !$template_path ) {
+  				$this->findTemplate();
 				} else {
   				$this->template_path = $template_path;
 				}
 			}
 			
 			private function findTemplate() {
-        if( file_exists( get_template_directory() . '/timeline-events/' . $this->slug . $file . '.php' ) ) {
-    			$this->template_path = get_template_directory() . '/timeline-events/' . $this->slug . $file . '.php';
-    		} elseif( file_exists( dirname(__FILE__) . '/timeline-events/' . $this->slug . $file . '.php' ) ) {
-      		$this->template_path =  dirname(__FILE__) . '/timeline-events/' . $this->slug . $file . '.php';
+        if( file_exists( get_template_directory() . '/timeline-events/' . $this->slug . '.php' ) ) {
+    			$this->template_path = get_template_directory() . '/timeline-events/' . $this->slug . '.php';
+    		} elseif( file_exists( dirname(__FILE__) . '/timeline-events/' . $this->slug . '.php' ) ) {
+      		$this->template_path =  dirname(__FILE__) . '/timeline-events/' . $this->slug . '.php';
     		} else {
       		$this->template_path = dirname(__FILE__) . '/timeline-events/timeline-event.php';
     		}
