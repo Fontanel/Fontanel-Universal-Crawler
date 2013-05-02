26e15f51aa0ad73c0348ff024aa1185e5d1d9ac6
Jasper Kennis <jasper@fontanel.nl>
2013-05-02 16:14:20 +0200

    Created a default template with BIER!

--- fontanel-universal-crawler-timeline-event.class.php
+++ fontanel-universal-crawler-timeline-event.class.php
@@ -17,15 +17,22 @@
 			private function findTemplate() {
         if( file_exists( get_template_directory() . '/timeline-events/' . $this->slug . '.php' ) ) {
     			$this->template_path = get_template_directory() . '/timeline-events/' . $this->slug . '.php';
-    		} elseif( file_exists( dirname(__FILE__) . '/timeline-events/' . $this->slug . '.php' ) ) {
-      		$this->template_path =  dirname(__FILE__) . '/timeline-events/' . $this->slug . '.php';
+    		} elseif( file_exists( dirname(__FILE__) . '/timeline-events-templates/' . $this->slug . '.php' ) ) {
+      		$this->template_path =  dirname(__FILE__) . '/timeline-event-templates/' . $this->slug . '.php';
     		} else {
-      		$this->template_path = dirname(__FILE__) . '/timeline-events/timeline-event.php';
+      		$this->template_path = dirname(__FILE__) . '/timeline-event-templates/timeline-event.php';
     		}
 			}
 			
 			public function render() {
-  			
+/*
+        if (is_array($vars) && !empty($vars)) {
+          extract($vars);
+        }
+*/
+  			ob_start();
+        include $this->template_path;
+        return ob_get_clean();
 			}
 		}
 	endif;
