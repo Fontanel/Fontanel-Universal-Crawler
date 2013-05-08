3083e28754e96caec6f24bc7b56a1b8f67248e91
Jasper Kennis <jasper@fontanel.nl>
2013-05-08 10:57:56 +0200

    Created default tumblr templates.

---
+++ timeline-events/fontanel-universal-crawler-timeline-event-tumblr-text.class.php
@@ -0,0 +1,8 @@
+<?php
+  require_once( FONTANEL_UNIVERSAL_CRAWLER_TIMELINE_EVENTS_DIR . '/fontanel-universal-crawler-timeline-event-tumblr.class.php' );
+  
+  if( ! class_exists( 'TimelineEventTumblrText' ) ):
+		class TimelineEventTumblrText extends TimelineEventTumblr {
+		}
+	endif;
+?>
