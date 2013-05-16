886edacbb1f82c8ef680e445ad882b61024166f7
Jasper Kennis <jasper@fontanel.nl>
2013-05-16 15:46:57 +0200

    Added basic elements to the goedbezig page.

--- timeline-event-templates/timeline-event-magazine-goedbezig.php
+++ timeline-event-templates/timeline-event-magazine-goedbezig.php
@@ -1,8 +1,18 @@
-<article class="story nieuwwerk">
+<?php
+  $portrait = wp_get_attachment_image_src( get_field( 'portretfoto', $vars['objects']->ID ), 'portrait_l' );
+	$logo_l = wp_get_attachment_image_src( get_field( 'logo_l', $vars['objects']->ID ), 'original' );
+	$logo_s = wp_get_attachment_image_src( get_field( 'logo_s', $vars['objects']->ID ), 'original' );
+	$bg = get_field( 'bg_pattern', $vars['objects']->ID );
+?>
+<article class="story goedbezig" style="background-image: url(<?php print( $bg ); ?>);">
 	<div class="article-body">
 		<div class="caption">
-			<h2><?php print_r($vars['type']); ?> (<?php print_r($vars['slug']); ?>)</h2>
-      <p><?php print_r($vars['objects']); ?></p>
+		  <img src="<?php print( $portrait[0] ); ?>" ?>
+		  <img src="<?php print( $logo_l[0] ); ?>" ?>
+			<h2><?php print( $vars['objects']->post_title ); ?></h2>
+			<h3>post_excerpt</h3>
+			<div style="width: 100px; height: 100px; background-color: <?php print($rgb) ?>;">
+      <!-- <p><?php print_r($vars['objects']); ?></p> -->
 		</div>
 	</div>
 </article>
