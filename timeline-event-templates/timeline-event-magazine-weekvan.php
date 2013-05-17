5987a00c52e245aa57026966e2866041b3ce378e
Jasper Kennis <jasper@fontanel.nl>
2013-05-17 14:34:22 +0200

    Filling the week van with data.

--- timeline-event-templates/timeline-event-magazine-weekvan.php
+++ timeline-event-templates/timeline-event-magazine-weekvan.php
@@ -16,23 +16,21 @@
   		)
   	);
 	} 
+	$portrait = wp_get_attachment_image_src( get_field('portretfoto', $vars['objects']->ID), 'medium' );
 ?>
 <article class="story weekvan">
-	<div class="article-body">
-		<div class="caption">
-      <h2><?php print( $vars['objects']->post_title ); ?></h2>
-      <h3><?php print( $vars['objects']->post_excerpt ); ?></h3>
-      <?php $portrait = wp_get_attachment_image_src( get_field('portretfoto', $vars['objects']->ID), 'medium' ); ?>
-		  <img src="<?php echo $portrait[0] ?>" class="portrait">
-      <?php
-				if ( count( $attachments ) > 0 ) {
-					foreach ( $attachments as $attachment ) {
-						$title = $attachment->post_title;
-						echo '<li><a href="#' . strtolower( $title ) . '">' . $title . '</a></li>';
-						}
-					}
-				?>
-<!-- 				<p><?php print_r($vars['objects']); ?></p> -->
-		</div>
-	</div>
+  <img src="<?php echo $portrait[0] ?>" class="portrait">
+  <img src="<?php bloginfo('template_directory') ?>/img/format-weekvan-logo-l.png" class="logo" />
+  <h2><?php print( $vars['objects']->post_title ); ?></h2>
+  <h3><?php print( $vars['objects']->post_excerpt ); ?></h3>
+  <?php
+		if ( count( $attachments ) > 0 ) {
+			foreach ( $attachments as $attachment ) {
+				$title = $attachment->post_title;
+				echo '<li><a href="#' . strtolower( $title ) . '">' . $title . '</a></li>';
+				}
+			}
+		?>
+  <a href="<?php print( get_permalink( $vars['objects']->ID ) ); ?>">Bekijk de story</a>
+  <!-- <?php print_r($vars['objects']); ?> -->
 </article>
