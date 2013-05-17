94473510df87139c7558edc9f0a856d629581350
Jasper Kennis <jasper@fontanel.nl>
2013-05-17 16:17:39 +0200

    Only author if one is there at all.

--- timeline-event-templates/partials/author.php
+++ timeline-event-templates/partials/author.php
@@ -1,11 +1,8 @@
+<?php if( !is_null( $vars['user'] ) ): ?>
 <aside class="avatar">
   <figure>
-    <?php if( !is_null( $vars['user'] ) ): ?>
-      <img src="<?php print( $vars['user']->thumb ); ?>"?>
-      <figcaption><?php print( $vars['user']->name ); ?></figcaption>
-    <?php else: ?>
-      <img src="unkown_author.png">
-      <figcaption>Annoniem</figcaption>
-    <?php endif; ?>
+    <img src="<?php print( $vars['user']->thumb ); ?>">
+    <figcaption><?php print( $vars['user']->name ); ?></figcaption>
   </figure>
 </aside>
+<?php endif; ?>
