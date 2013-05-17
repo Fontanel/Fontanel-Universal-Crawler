93b4da8a04942b7789c18390c250680026cb2a94
Jasper Kennis <jasper@fontanel.nl>
2013-05-17 15:51:08 +0200

    Don't allow fontanel posts to ruine the entire layout.

--- timeline-event-templates/timeline-event-magazine-fontanel.php
+++ timeline-event-templates/timeline-event-magazine-fontanel.php
@@ -2,7 +2,7 @@
 	<div class="article-body">
 		<div class="caption">
 			<h2><?php print_r($vars['type']); ?> (<?php print_r($vars['slug']); ?>)</h2>
-      <p><?php print_r($vars['objects']); ?></p>
+      <!-- <?php print_r($vars['objects']); ?> -->
 		</div>
 	</div>
 </article>
