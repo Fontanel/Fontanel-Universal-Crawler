1c40da18999391fc01edb2faecccca979340b961
Jasper Kennis <jasper@fontanel.nl>
2013-05-23 14:24:01 +0200

    Important set of bug fixes.

--- crawlers/fontanel-universal-crawler-timeline-jobs-weekly-roundup-crawler.class.php
+++ crawlers/fontanel-universal-crawler-timeline-jobs-weekly-roundup-crawler.class.php
@@ -1,7 +1,7 @@
 <?php
 	if( ! class_exists( 'TimelineJobsWeeklyRoundupCrawler' ) ):
 		class TimelineJobsWeeklyRoundupCrawler extends TimelineCrawler {
-			private $platform = 'jobs_roundup';
+			private $platform = 'jobs';
 			
 			public function fetchPosts() {
 				$result = $this->fetch( 'http://fontaneljobs.nl/roundup.json' );
@@ -12,19 +12,23 @@
 			private function processResult( $result ) {
 				$workable_result = json_decode( $result );
 				
-				$object_id = $workable_result[0]->id;
-				$type_id = $this->getTypeId( $this->platform );
+				$mapper = function($val) {
+  				$val->id;
+				};
+				
+				$object_ids = array_map($mapper, $workable_result );
+				$type_id = $this->getTypeId( $this->platform, 'roundup' );
 				$timestamp = $workable_result[0]->created_at;
 				
 				$savable_objects = $this->createSavableObjects( $workable_result );
 				
-				$this->storeEvent( $type_id, $object_id, $timestamp, $savable_objects );
+				$this->storeEvent( $type_id, $object_ids, $timestamp, $savable_objects );
 			}
 			
 			private function createSavableObjects( $raw_objects ) {
 				$results = array();
 				foreach( $raw_objects as $raw_object ) {
-				  if( $raw_object['type'] == 'fulltime' ) {
+				  if( $raw_object['type'] == 'Vaste baan' ) {
   					$new_savable_object = array();
   					$new_savable_object['type'] = 'jobs';
   					$new_savable_object['id'] = $raw_object->id;
