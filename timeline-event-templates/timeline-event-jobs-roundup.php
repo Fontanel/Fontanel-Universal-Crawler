f75bd098356e66b5ada0f9585846b941cd869bb5
Jasper Kennis <jasper@fontanel.nl>
2013-05-23 16:13:38 +0200

    Enable proper laying out of the jobs in a roundup.

--- timeline-event-templates/timeline-event-jobs-roundup.php
+++ timeline-event-templates/timeline-event-jobs-roundup.php
@@ -3,6 +3,27 @@
 	<div class="article-body">
   	<h2><a href="http://www.fontaneljobs.nl">Weekly roundup</a></h2>
   	<h3>Alle vacatures van de afgelopen week</h3>
-		<?php print_r($vars['objects']); ?>
+  	<ul>
+  	  <?php foreach( $vars['objects'] as $raw_job ): ?>
+  	    <?php $job = json_decode( $raw_job->object ); ?>
+        <li>
+          <a href="/vacature/senior-designer-on-en-offline" target="_blank">
+            <h5><?php print( $job->company ); ?> zoekt</h5>
+            <h4><?php print( $job->job_function ); ?></h4>
+            <p>
+              Vaste baan in Den Haag E.O.
+              <span class="fields-container">
+                <span class="dot">•</span>
+                Grafisch ontwerp + Reclame
+              </span>
+              <span class="dot">•</span>
+              17 mei 2013
+            </p>
+          </a>
+        </li>
+        <!-- <?php print_r( $job ); ?> -->
+  	  <?php endforeach; ?>
+  	</ul>
+
   </div>
 </article>
