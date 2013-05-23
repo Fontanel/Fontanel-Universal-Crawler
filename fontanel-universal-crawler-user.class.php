73a5ce21f4384fff98dfdb7ac0b913040efd1d5a
Jasper Kennis <jasper@fontanel.nl>
2013-05-23 16:16:18 +0200

    Ability to add a url to the user property.

--- fontanel-universal-crawler-user.class.php
+++ fontanel-universal-crawler-user.class.php
@@ -2,13 +2,15 @@
 	if( ! class_exists( 'FontanelUniversalCrawlerUser' ) ):
   	class FontanelUniversalCrawlerUser {
   	  public $name = '';
-  	  public $thumb_id = '';
+  	  public $thumb = '';
+  	  public $url = '';
   	  private $wordpress_id = 0;
   	    	  
-    	public function __construct( $name = '', $thumb = '', $wordpress_id = 0 ) {
+    	public function __construct( $name = '', $thumb = '', $wordpress_id = 0, $url = '' ) {
         $this->name = $name;
         $this->thumb = $thumb;
         $this->wordpress_id = $wordpress_id;
+        $this->url = $url;
 			}
   	}
   endif;
