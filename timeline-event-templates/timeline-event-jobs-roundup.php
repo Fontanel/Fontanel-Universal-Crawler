b958ea8e891e9dce1ef81f3af4f51220b10cc7b5
Jasper Kennis <jasper@fontanel.nl>
2013-05-23 15:12:29 +0200

    Created the classes needed to be able to show the jobs.

---
+++ timeline-event-templates/timeline-event-jobs-roundup.php
@@ -0,0 +1,8 @@
+<article class="jobs weekly">
+	<?php include( dirname(__FILE__) . '/partials/author.php' ); ?>
+	<div class="article-body">
+  	<h2><a href="http://www.fontaneljobs.nl">Weekly roundup</a></h2>
+  	<h3>Alle vacatures van de afgelopen week</h3>
+		<?php print_r($vars['objects']); ?>
+  </div>
+</article>
