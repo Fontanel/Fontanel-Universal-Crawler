08ca8593fc57d8e0c9320f0a3fb1e42d66592e6d
Jasper Kennis <jasper@fontanel.nl>
2013-05-24 17:24:37 +0200

    Moved gradient styles in a partial and use them on newwerk.

--- timeline-event-templates/timeline-event-magazine-nieuwwerk-carousel.php
+++ timeline-event-templates/timeline-event-magazine-nieuwwerk-carousel.php
@@ -1,5 +1,7 @@
-<div class="item story nieuwwerk" style="background-image: url('<?php the_field( 'achtergrondafbeelding', $vars['objects']->ID ); ?>');" id="post-<?php print( $vars['objects']->ID ); ?>" <?php include( dirname(__FILE__) . '/partials/carousel-colors.php' ); ?>>
+<div class="item story nieuwwerk post-<?php print( $vars['objects']->ID ); ?>" style="background-image: url('<?php the_field( 'achtergrondafbeelding', $vars['objects']->ID ); ?>');" <?php include( dirname(__FILE__) . '/partials/carousel-colors.php' ); ?>>
 	<h2><?php print( $vars['objects']->post_title ); ?></h2>
 	<h3><?php print( $vars['objects']->post_excerpt ); ?></h3>
 	<?php include( dirname(__FILE__) . '/partials/carousel-next.php' ); ?>
 </div>
+
+<?php include( dirname(__FILE__) . '/partials/gradient-bg.php' ); ?>
