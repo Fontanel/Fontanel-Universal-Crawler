b48dfe217d31f98e878fc0a8a21409913cccea10
Jasper Kennis <jasper@fontanel.nl>
2013-05-24 12:06:56 +0200

    Showing 4 images in the carousel, not one.

--- timeline-event-templates/timeline-event-magazine-weekvan-carousel.php
+++ timeline-event-templates/timeline-event-magazine-weekvan-carousel.php
@@ -4,20 +4,26 @@
   $attachments = array();
 		
   if( count( $ids ) > 0 ) {
+    global $keys;
+    $mapper = function($key){
+      global $keys;
+      return $keys[$key];
+    };
+    
     $keys = explode( ',', $ids[1] );
-    $random_key = array_rand( $keys );
-    $random_val = $keys[$random_key];
+    $random_keys = array_rand( $keys, 4 );
+    $random_vals = array_map( $mapper, $random_keys );
     $attachments = get_posts( array(
   		'post_type' => 'attachment',
-  		'posts_per_page' => 1,
-  		'include' => $random_val,
+  		'posts_per_page' => 4,
+  		'include' => $random_vals,
   		'exclude' => array(
   									get_field( 'portret_persoon', $vars['objects']->ID ),
   									get_field( 'grote_afbeelding', $vars['objects']->ID )
   								)
   		)
   	);
-	} 
+	}
 	$portrait = wp_get_attachment_image_src( get_field('portretfoto', $vars['objects']->ID), 'medium' );
 ?>
 
@@ -26,20 +32,22 @@
   <img src="<?php bloginfo('template_directory') ?>/img/format-weekvan-logo-l.png" class="logo" />
   <?php
 		if ( count( $attachments ) > 0 ):
-			foreach ( $attachments as $attachment ): ?>
-				<?php print( wp_get_attachment_image(
+		  $i = 0;
+			foreach ( $attachments as $attachment ):
+				print( wp_get_attachment_image(
 				  $attachment->ID,
-				  'portrait_s',
+				  array( 240, 240 ),
 				  false,
 				  array(
 				    'class' => 'prev-thumb',
-				    'style' => 'right: -' . rand( 1, 4 ) . 'rem; top: -' . rand( 1, 4 ) . 'rem; transform:rotate(' . rand( -25, 25 ) . 'deg);-ms-transform:rotate(' . rand( -25, 25 ) . 'deg);-webkit-transform:rotate(' . rand( -25, 25 ) . 'deg);'
+				    'style' => 'right: ' . rand( -1 * $i, 5 * $i ) . 'rem; top: ' . rand( -2, 4 ) . 'rem; z-index: ' . rand( 1, 4 ) . '; transform:rotate(' . rand( -25, 25 ) . 'deg);-ms-transform:rotate(' . rand( -25, 25 ) . 'deg);-webkit-transform:rotate(' . rand( -25, 25 ) . 'deg);'
 				  ) 
-				) ); ?>
-  		<?php endforeach;
+				) );
+				$i++;
+  		endforeach;
 		endif;
 	?>
   <h2><?php print( $vars['objects']->post_title ); ?></h2>
   <h3><?php print( $vars['objects']->post_excerpt ); ?></h3>
-  <a href="<?php print( get_permalink( $vars['objects']->ID ) ); ?>">Bekijk de story</a>
+  <a class="next" href="#"></a>
 </div>
