e9ecb7cf857eca1e44e61a592cfd126566734d2c
Jasper Kennis <jasper@fontanel.nl>
2013-05-24 15:16:33 +0200

    Embedded the ability to show the black class.

--- timeline-event-templates/partials/carousel-colors.php
+++ timeline-event-templates/partials/carousel-colors.php
@@ -1,1 +1,1 @@
-data-second-background="<?php print_r( get_field( 'tweede_kleur', $vars['objects']->ID ) ); ?>" data-logo-background="<?php print_r( get_field( 'eerste_kleur', $vars['objects']->ID ) ); ?>"
+data-second-background="<?php print_r( get_field( 'tweede_kleur', $vars['objects']->ID ) ); ?>" data-logo-background="<?php print_r( get_field( 'eerste_kleur', $vars['objects']->ID ) ); ?>" data-thema="<?php print_r( get_field( 'thema', $vars['objects']->ID ) ); ?>"
