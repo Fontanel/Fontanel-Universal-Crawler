44b805fd78bed88161cf4b5dcd49b19a25b09e45
Jasper Kennis <jasper@fontanel.nl>
2013-06-13 16:45:23 +0200

    Smarter read more reference

--- timeline-event-templates/timeline-event-tumblr.php
+++ timeline-event-templates/timeline-event-tumblr.php
@@ -1,5 +1,11 @@
-<?php $post = json_decode( $vars['objects'][0]->object ); ?>
-<?php $article_parts = preg_split("/<p><!-- more -->.*?\/p>/", $post->body ); ?>
+<?php
+  $post = json_decode( $vars['objects'][0]->object );
+  if( !$vars['skip_readmore_wrap'] ) {
+    $article_parts = preg_split("/<p><!-- more -->.*?\/p>/", $post->body );
+  } else {
+    $article_parts = array( $post->body );
+  }
+?>
 <article class="note text">
 	<?php include( dirname(__FILE__) . '/partials/author.php' ); ?>
 	<div class="article-body">
