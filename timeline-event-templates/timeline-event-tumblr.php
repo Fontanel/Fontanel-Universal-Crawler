23d05a15a5e6c66af0f7170f5fc6384688719eb4
Jasper Kennis <jasper@fontanel.nl>
2013-06-18 14:46:56 +0200

    Show titles in tumblr text messages.

--- timeline-event-templates/timeline-event-tumblr.php
+++ timeline-event-templates/timeline-event-tumblr.php
@@ -9,7 +9,12 @@
 <article class="note text">
 	<?php include( dirname(__FILE__) . '/partials/author.php' ); ?>
 	<div class="article-body">
-  	<div class="text"><?php print( $article_parts[0] ); ?></div>
+  	<div class="text">
+    	<?php if( !empty( $post->title ) ): ?>
+        <h2><?php print( $post->title ); ?></h2>
+      <?php endif; ?>
+      <?php print( $article_parts[0] ); ?>
+    </div>
   	<?php include( dirname(__FILE__) . '/partials/read-more.php' ); ?>
 		<?php include( dirname(__FILE__) . '/partials/footer.php' ); ?>
 	</div>
