c99c1270b00b01e694d0adf72859d43df3f0cd94
Jasper Kennis <jasper@fontanel.nl>
2013-06-24 13:21:22 +0200

    Created initial presented by partial.

---
+++ timeline-event-templates/partials/sponsor.php
@@ -0,0 +1,6 @@
+<?php if( !is_null( $vars ) ): ?>
+  <figure class="sponsor">
+    <figcaption>presented by</figcaption>
+    <img src="<?php bloginfo('template_directory') ?>/img/vitra-logo-green.png">
+  </figure>
+<?php endif; ?>
