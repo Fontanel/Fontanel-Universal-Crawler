06114750c5a2dd822bb5cc64235872356b07ff4e
Jasper Kennis <jasper@fontanel.nl>
2013-06-24 13:26:51 +0200

    Send in the sponsor property.

--- fontanel-universal-crawler.class.php
+++ fontanel-universal-crawler.class.php
@@ -141,9 +141,9 @@
     		  }
     		  
     		  if( class_exists( $class_name ) ) {
-      		  return new $class_name( $event->id, $event->objects, $this->database_manager, $type, false, $user );
+      		  return new $class_name( $event->id, $event->objects, $this->database_manager, $type, false, $user, $sponsor );
     		  } else {
-        		return new TimelineEvent( $event->id, $event->objects, $this->database_manager, $type, false, $user );
+        		return new TimelineEvent( $event->id, $event->objects, $this->database_manager, $type, false, $user, $sponsor );
       		}
     		}
   		}
