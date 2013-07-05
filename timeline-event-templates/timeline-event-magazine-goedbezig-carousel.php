2c6dd8bf434a5b17649603a598fd134a71989ae8
Jasper Kennis <info@jasperkennis.nl>
2013-07-05 16:20:36 +0200

    Assigning the first image from the goedbezig.

--- timeline-event-templates/timeline-event-magazine-goedbezig-carousel.php
+++ timeline-event-templates/timeline-event-magazine-goedbezig-carousel.php
@@ -1,10 +1,8 @@
-<?php
-	$logo_l = wp_get_attachment_image_src( get_field( 'logo_l', $vars['objects']->ID ), 'original' );
-	$bg = get_field( 'bg_pattern', $vars['objects']->ID );
-?>
-<a class="item story goedbezig" style="background-image: url(<?php print( $bg ); ?>);" <?php include( dirname(__FILE__) . '/partials/carousel-colors.php' ); ?>>
+<a class="item story nieuwwerk goedbezig post-<?php print( $vars['objects']->ID ); ?>" style="background-image: url('<?php include( dirname(__FILE__) . '/partials/single-acf-attachment-image.php' ); ?>');" <?php include( dirname(__FILE__) . '/partials/carousel-colors.php' ); ?>>
   <?php include( dirname(__FILE__) . '/partials/sponsor.php' ); ?>
-  <img src="<?php print( $logo_l[0] ); ?>" class="logo">
 	<h2><?php print( $vars['objects']->post_title ); ?></h2>
 	<h3><?php print( $vars['objects']->post_excerpt ); ?></h3>
 </a>
+
+<?php include( dirname(__FILE__) . '/partials/good-bezig-logo-bg.php' ); ?>
+<?php include( dirname(__FILE__) . '/partials/gradient-bg.php' ); ?>
