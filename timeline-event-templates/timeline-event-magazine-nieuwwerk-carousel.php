17814677b4c3cf49d825e34e0d6befa091d4cbde
Jasper Kennis <info@jasperkennis.nl>
2013-07-05 11:02:19 +0200

    Adding a label class to the weekvan stories that should have the 3vragenaan label.

--- timeline-event-templates/timeline-event-magazine-nieuwwerk-carousel.php
+++ timeline-event-templates/timeline-event-magazine-nieuwwerk-carousel.php
@@ -1,4 +1,4 @@
-<a class="item story nieuwwerk post-<?php print( $vars['objects']->ID ); ?>" style="background-image: url('<?php the_field( 'achtergrondafbeelding', $vars['objects']->ID ); ?>');" <?php include( dirname(__FILE__) . '/partials/carousel-colors.php' ); ?>>
+<a class="item story nieuwwerk label post-<?php print( $vars['objects']->ID ); ?>" style="background-image: url('<?php the_field( 'achtergrondafbeelding', $vars['objects']->ID ); ?>');" <?php include( dirname(__FILE__) . '/partials/carousel-colors.php' ); ?>>
   <?php include( dirname(__FILE__) . '/partials/sponsor.php' ); ?>
 	<h2><?php print( $vars['objects']->post_title ); ?></h2>
 	<h3><?php print( $vars['objects']->post_excerpt ); ?></h3>
