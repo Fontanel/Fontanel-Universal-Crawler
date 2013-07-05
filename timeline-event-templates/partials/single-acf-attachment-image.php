4b37948280d0929f255e867ef38ed71c8cee4810
Jasper Kennis <info@jasperkennis.nl>
2013-07-05 16:27:57 +0200

    Limitting result in the attachment image partial to 1 url.

--- timeline-event-templates/partials/single-acf-attachment-image.php
+++ timeline-event-templates/partials/single-acf-attachment-image.php
@@ -7,8 +7,12 @@
         if ( get_sub_field('afbeeldingen') ) {
           $inner_first = true;
           foreach( get_sub_field('afbeeldingen') as $image ){
-            $projectImg = wp_get_attachment_image_src( $image['afbeelding'] );
-						print( $projectImg[0] );
+            if( $inner_first ) {
+              $projectImg = wp_get_attachment_image_src( $image['afbeelding'] );
+  						print( $projectImg[0] );
+  						$first = false;
+  						$inner_first = false;
+    				}
           }
         }
       }
