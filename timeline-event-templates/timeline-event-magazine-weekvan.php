aa0b7ad3d4c821c930db91312068f4e545c0e1f7
Jasper Kennis <info@jasperkennis.nl>
2013-07-05 11:10:48 +0200

    Conditionalised displaying the fixed sponsor.

--- timeline-event-templates/timeline-event-magazine-weekvan.php
+++ timeline-event-templates/timeline-event-magazine-weekvan.php
@@ -1,9 +1,13 @@
 <article class="story weekvan">
   <img src="<?php echo $portrait[0] ?>" class="portrait">
-  <figure class="sponsor">
-    <figcaption>presented by</figcaption>
-    <img src="<?php bloginfo('template_directory') ?>/img/vitra-logo-purple.png">
-  </figure>
+  <?php if( is_null( $vars['sponsor'] ) ): ?>
+    <figure class="sponsor">
+      <figcaption>presented by</figcaption>
+      <img src="<?php bloginfo('template_directory') ?>/img/vitra-logo-purple.png">
+    </figure>  
+  <?php else: ?>
+    <?php include( dirname(__FILE__) . '/partials/sponsor.php' ); ?>
+  <?php endif; ?>
   <img src="<?php bloginfo('template_directory') ?>/img/logo-weekvan.png" class="logo" />
   <h2><?php print( $vars['objects']->post_title ); ?></h2>
   <h3>Chief Pencil bij Present Plus</h3>
