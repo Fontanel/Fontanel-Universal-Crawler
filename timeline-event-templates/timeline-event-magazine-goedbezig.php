93587250f9127d066bcd2bc1a5771c51c5c5860d
Jasper Kennis <info@jasperkennis.nl>
2013-07-05 14:36:39 +0200

    Adjusted the goedbezig layout to match the 3vragenaan layout.

--- timeline-event-templates/timeline-event-magazine-goedbezig.php
+++ timeline-event-templates/timeline-event-magazine-goedbezig.php
@@ -1,11 +1,9 @@
-<?php
-	$logo_l = wp_get_attachment_image_src( get_field( 'logo_l', $vars['objects']->ID ), 'original' );
-	$bg = get_field( 'bg_pattern', $vars['objects']->ID );
-?>
-<article class="story goedbezig" style="background-image: url(<?php print( $bg ); ?>);">
+<article class="story nieuwwerk goedbezig post-<?php print( $vars['objects']->ID ); ?>" style="background-image: url('<?php the_field( 'achtergrondafbeelding', $vars['objects']->ID ); ?>');" id="post-<?php print( $vars['objects']->ID ); ?>">
   <?php include( dirname(__FILE__) . '/partials/sponsor.php' ); ?>
-  <img src="<?php print( $logo_l[0] ); ?>" class="logo">
 	<h2><?php print( $vars['objects']->post_title ); ?></h2>
 	<h3><?php print( $vars['objects']->post_excerpt ); ?></h3>
-	<a href="<?php print( get_permalink( $vars['objects']->ID ) ); ?>" class="deep">Bekijk de story</a>
+	<a href="<?php print( get_permalink( $vars['objects']->ID ) ); ?>" style="background-color: <?php the_field( 'header_kleur_boven', $vars['objects']->ID ) ?>;" class="deep">Bekijk de story</a>
 </article>
+
+<?php include( dirname(__FILE__) . '/partials/good-bezig-logo-bg.php' ); ?>
+<?php include( dirname(__FILE__) . '/partials/gradient-bg.php' ); ?>
