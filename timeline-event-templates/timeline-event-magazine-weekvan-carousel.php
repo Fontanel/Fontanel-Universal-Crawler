7ceae78c04f0618c6765ecc95b2da84c993a1122
Jasper Kennis <info@jasperkennis.nl>
2013-07-05 11:10:15 +0200

    Removed logic that has become irrelevant to implementation.

--- timeline-event-templates/timeline-event-magazine-weekvan-carousel.php
+++ timeline-event-templates/timeline-event-magazine-weekvan-carousel.php
@@ -1,33 +1,3 @@
-<?php
-  $post_content = $vars['objects']->post_content;
-	preg_match('/\[gallery.*ids=.(.*).\]/', $post_content, $ids);
-  /*
-$attachments = array();
-		
-  if( count( $ids ) > 0 ) {
-    global $keys;
-    $mapper = function($key){
-      global $keys;
-      return $keys[$key];
-    };
-    
-    $keys = explode( ',', $ids[1] );
-    $random_keys = array_rand( $keys, 3 );
-    $random_vals = array_map( $mapper, $random_keys );
-    $attachments = get_posts( array(
-  		'post_type' => 'attachment',
-  		'posts_per_page' => 3,
-  		'include' => $random_vals,
-  		'exclude' => array(
-  									get_field( 'portret_persoon', $vars['objects']->ID ),
-  									get_field( 'grote_afbeelding', $vars['objects']->ID )
-  								)
-  		)
-  	);
-	}
-	$portrait = wp_get_attachment_image_src( get_field('portretfoto', $vars['objects']->ID), 'medium' );
-*/
-?>
 <a href="<?php print( get_permalink( $vars['objects']->ID ) ); ?>" class="item story weekvan" data-second-background="#300747">
   <?php /* <img src="<?php echo $portrait[0] ?>" class="portrait"> */ ?>
   <figure class="sponsor">
@@ -35,24 +5,6 @@ $attachments = array();
     <img src="<?php bloginfo('template_directory') ?>/img/vitra-logo-purple.png">
   </figure>
   <img src="<?php bloginfo('template_directory') ?>/img/logo-weekvan.png" class="logo" />
-  <?php
-		/*
-if ( count( $attachments ) > 0 ):
-		  $i = 0;
-			foreach ( $attachments as $attachment ):
-				print( wp_get_attachment_image(
-				  $attachment->ID,
-				  array( 240, 240 ),
-				  false,
-				  array(
-				    'class' => 'prev-thumb'
-				  ) 
-				) );
-				$i++;
-  		endforeach;
-		endif;
-*/
-	?>
   <h2><?php print( $vars['objects']->post_title ); ?></h2>
   <h3>Chief Pencil bij Present Plus</h3>
   <h4><?php print( $vars['objects']->post_excerpt ); ?></h4>
--- timeline-event-templates/timeline-event-magazine-weekvan.php
+++ timeline-event-templates/timeline-event-magazine-weekvan.php
@@ -1,27 +1,3 @@
-<?php
-  $post_content = $vars['objects']->post_content;
-	/*
-preg_match('/\[gallery.*ids=.(.*).\]/', $post_content, $ids);
-  $attachments = array();
-		
-  if( count( $ids ) > 0 ) {
-    $keys = explode( ',', $ids[1] );
-    $random_key = array_rand( $keys );
-    $random_val = $keys[$random_key];
-    $attachments = get_posts( array(
-  		'post_type' => 'attachment',
-  		'posts_per_page' => 1,
-  		'include' => $random_val,
-  		'exclude' => array(
-  									get_field( 'portret_persoon', $vars['objects']->ID ),
-  									get_field( 'grote_afbeelding', $vars['objects']->ID )
-  								)
-  		)
-  	);
-	}
-*/ 
-	$portrait = wp_get_attachment_image_src( get_field('portretfoto', $vars['objects']->ID), 'medium' );
-?>
 <article class="story weekvan">
   <img src="<?php echo $portrait[0] ?>" class="portrait">
   <figure class="sponsor">
@@ -29,23 +5,6 @@ preg_match('/\[gallery.*ids=.(.*).\]/', $post_content, $ids);
     <img src="<?php bloginfo('template_directory') ?>/img/vitra-logo-purple.png">
   </figure>
   <img src="<?php bloginfo('template_directory') ?>/img/logo-weekvan.png" class="logo" />
-  <?php
-		/*
-if ( count( $attachments ) > 0 ):
-			foreach ( $attachments as $attachment ): ?>
-				<?php print( wp_get_attachment_image(
-				  $attachment->ID,
-				  'portrait_s',
-				  false,
-				  array(
-				    'class' => 'prev-thumb',
-				    'style' => 'right: -' . rand( 1, 4 ) . 'rem; top: -' . rand( 1, 4 ) . 'rem; transform:rotate(' . rand( -25, 25 ) . 'deg);-ms-transform:rotate(' . rand( -25, 25 ) . 'deg);-webkit-transform:rotate(' . rand( -25, 25 ) . 'deg);'
-				  ) 
-				) ); ?>
-  		<?php endforeach;
-		endif;
-*/
-	?>
   <h2><?php print( $vars['objects']->post_title ); ?></h2>
   <h3>Chief Pencil bij Present Plus</h3>
   <h4><?php print( $vars['objects']->post_excerpt ); ?></h4>
