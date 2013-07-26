5774e62ce31e36502556d76c3974be4e66ea4fba
Jasper Kennis <info@jasperkennis.nl>
2013-07-26 15:44:23 +0200

    Make sure the archive header class doesnt interfere with the later defined normal one.

--- timeline-event-templates/timeline-event-magazine-goedbezig-archive.php
+++ timeline-event-templates/timeline-event-magazine-goedbezig-archive.php
@@ -1,11 +1,11 @@
-<div class="item story nieuwwerk goedbezig archive post-<?php print( $vars['objects']->ID ); ?>" 
+<div class="item story nieuwwerk goedbezig archive post-<?php print( $vars['objects']->ID ); ?>-archive" 
 style="background-image: url('<?php print( get_field( 'bg_pattern', $vars['objects']->ID ) ); ?>');">
   <?php include( dirname(__FILE__) . '/partials/sponsor.php' ); ?>
 	<h2>Een podium waar verfrissend werk van nieuw Nederlands talent getoond word</h2>
 </div>
 
 <style>
-  .post-<?php print( $vars['objects']->ID ); ?>:before {
+  .post-<?php print( $vars['objects']->ID ); ?>-archive:before {
 	  background-image: url("<?php echo wp_get_attachment_image_src( get_field( 'logo_l', $vars['objects']->ID ), 'original' )[0]; ?>");
 	}
 </style>
