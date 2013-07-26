b3339a5a346e20d12253e5365c095636342a08e3
Jasper Kennis <info@jasperkennis.nl>
2013-07-26 17:32:12 +0200

    Cleaning what element has what styles and classes.

--- timeline-event-templates/timeline-event-magazine-nieuwwerk-archive.php
+++ timeline-event-templates/timeline-event-magazine-nieuwwerk-archive.php
@@ -2,12 +2,3 @@
   <?php include( dirname(__FILE__) . '/partials/sponsor.php' ); ?>
 	<h2>Een persoonlijke plek voor nieuwe projecten en initiatieven achter de schermen.</h2>
 </div>
-
-<style>
-  .post-<?php print( $vars['objects']->ID ); ?>:after {
-	  background-image: -webkit-radial-gradient( center top, circle cover, rgba( 255, 255, 255, .8 ) 0rem, rgba( 255, 255, 255, 1 ) 100% );
-	  background-image: -moz-radial-gradient( center top, circle cover, rgba( 255, 255, 255, .8 ) 0%, rgba( 255, 255, 255, 1 ) 100% );
-	  background-image: -o-radial-gradient( center top, circle cover, rgba( 255, 255, 255, .8 ) 0%, rgba( 255, 255, 255, 1 ) 100% );
-	  background-image: radial-gradient( center top, circle cover, rgba( 255, 255, 255, .8 ) 0%, rgba( 255, 255, 255, 1 ) 100% );
-	}
-</style>
