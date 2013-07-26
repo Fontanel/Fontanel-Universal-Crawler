52437596016e634559e2e1810f0a9bd6f927c76d
Jasper Kennis <info@jasperkennis.nl>
2013-07-26 17:49:44 +0200

    Removing double class defenition.

--- timeline-event-templates/timeline-event-magazine-goedbezig-carousel.php
+++ timeline-event-templates/timeline-event-magazine-goedbezig-carousel.php
@@ -1,4 +1,4 @@
-<a class="item story nieuwwerk goedbezig carousel header post-<?php print( $vars['objects']->ID ); ?>" style="background-image: url('<?php include( dirname(__FILE__) . '/partials/single-acf-attachment-image.php' ); ?>');" <?php include( dirname(__FILE__) . '/partials/carousel-colors.php' ); ?> data-id="<?php print_r( $vars['id'] ); ?>">
+<a class="item story goedbezig carousel header post-<?php print( $vars['objects']->ID ); ?>" style="background-image: url('<?php include( dirname(__FILE__) . '/partials/single-acf-attachment-image.php' ); ?>');" <?php include( dirname(__FILE__) . '/partials/carousel-colors.php' ); ?> data-id="<?php print_r( $vars['id'] ); ?>">
   <?php include( dirname(__FILE__) . '/partials/sponsor.php' ); ?>
 	<h2><?php print( $vars['objects']->post_title ); ?></h2>
 	<h3><?php print( $vars['objects']->post_excerpt ); ?></h3>
