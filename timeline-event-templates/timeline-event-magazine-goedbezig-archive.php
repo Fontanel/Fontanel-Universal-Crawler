a620e49a565fc385085da04cd5f4f893a1c4e2c8
Jasper Kennis <info@jasperkennis.nl>
2013-07-26 15:33:00 +0200

    Fidling with paths.

--- timeline-event-templates/timeline-event-magazine-goedbezig-archive.php
+++ timeline-event-templates/timeline-event-magazine-goedbezig-archive.php
@@ -1,8 +1,11 @@
-<a class="item story nieuwwerk goedbezig post-<?php print( $vars['objects']->ID ); ?>" style="background-image: url('<?php include( dirname(__FILE__) . '/partials/single-acf-attachment-image.php' ); ?>');" <?php include( dirname(__FILE__) . '/partials/carousel-colors.php' ); ?> data-id="<?php print_r( $vars['id'] ); ?>">
+<div class="item story nieuwwerk goedbezig archive post-<?php print( $vars['objects']->ID ); ?>" 
+style="background-image: url('<?php print( get_field( 'bg_pattern', $vars['objects']->ID ) ); ?>');">
   <?php include( dirname(__FILE__) . '/partials/sponsor.php' ); ?>
-	<h2><?php print( $vars['objects']->post_title ); ?></h2>
-	<h3><?php print( $vars['objects']->post_excerpt ); ?></h3>
-</a>
+	<h2>Een podium waar verfrissend werk van nieuw Nederlands talent getoond word</h2>
+</div>
 
-<?php include( dirname(__FILE__) . '/partials/good-bezig-logo-bg.php' ); ?>
-<?php include( dirname(__FILE__) . '/partials/gradient-bg.php' ); ?>
+<style>
+  .post-<?php print( $vars['objects']->ID ); ?>:before {
+	  background-image: url("<?php echo wp_get_attachment_image_src( get_field( 'logo_l', $vars['objects']->ID ), 'original' )[0]; ?>");
+	}
+</style>
