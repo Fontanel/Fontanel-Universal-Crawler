65e1d842532e5f7d5791851f4405f90e558cb787
Jasper Kennis <info@jasperkennis.nl>
2013-08-05 17:23:01 +0200

    Simplefied the headers used in the archive pages.

--- timeline-event-templates/timeline-event-magazine-goedbezig-archive.php
+++ timeline-event-templates/timeline-event-magazine-goedbezig-archive.php
@@ -1,10 +1,3 @@
-<div class="item story goedbezig header post-<?php print( $vars['objects']->ID ); ?>-archive" style="background-image: url('<?php print( get_field( 'bg_pattern', $vars['objects']->ID ) ); ?>');">
+<div class="item story goedbezig header archive">
   <?php include( dirname(__FILE__) . '/partials/sponsor.php' ); ?>
-	<h2>Een podium waar verfrissend werk van nieuw Nederlands talent getoond word</h2>
 </div>
-
-<style>
-  .post-<?php print( $vars['objects']->ID ); ?>-archive:before {
-	  background-image: url("<?php echo wp_get_attachment_image_src( get_field( 'logo_l', $vars['objects']->ID ), 'original' )[0]; ?>");
-	}
-</style>
--- timeline-event-templates/timeline-event-magazine-nieuwwerk-archive.php
+++ timeline-event-templates/timeline-event-magazine-nieuwwerk-archive.php
@@ -1,4 +1,3 @@
-<div class="item story nieuwwerk label header post-<?php print( $vars['objects']->ID ); ?>" style="background-image: url('<?php the_field( 'achtergrondafbeelding', $vars['objects']->ID ); ?>');" <?php include( dirname(__FILE__) . '/partials/carousel-colors.php' ); ?> data-id="<?php print_r( $vars['id'] ); ?>">
+<div class="item story nieuwwerk label header archive">
   <?php include( dirname(__FILE__) . '/partials/sponsor.php' ); ?>
-	<h2>Een persoonlijke plek voor nieuwe projecten en initiatieven achter de schermen.</h2>
 </div>
