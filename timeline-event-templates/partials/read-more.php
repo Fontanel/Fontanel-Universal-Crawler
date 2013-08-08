ae0f9423f6503ff652b1032077035aa03ebb1dab
Jasper Kennis <info@jasperkennis.nl>
2013-08-08 16:07:18 +0200

    Dont use tags anymore.

--- timeline-event-templates/partials/read-more.php
+++ timeline-event-templates/partials/read-more.php
@@ -1,3 +1,3 @@
 <?php if( isset( $article_parts[1] ) ): ?>
-  <a href="<?php print( get_permalink( get_page_by_title( 'notes' ) ) ); ?>?u=<?php print($vars['objects'][0]->pretty_url); ?>">Lees meer</a>
+  &raquo; <a href="<?php print( get_permalink( get_page_by_title( 'notes' ) ) ); ?>?u=<?php print($vars['objects'][0]->pretty_url); ?>">Lees meer</a>
 <?php endif; ?>
