e0c061bb0cd81e16591befd0255fa9ad860f349f
Jasper Kennis <info@jasperkennis.nl>
2013-08-23 17:17:58 +0200

    Adjusted the carousel html for vertical centering.

--- timeline-event-templates/timeline-event-magazine-fontanel-carousel.php
+++ timeline-event-templates/timeline-event-magazine-fontanel-carousel.php
@@ -1,7 +1,9 @@
-<a class="item story nieuwwerk post-<?php print( $vars['objects']->ID ); ?>" style="background-image: url('<?php the_field( 'achtergrondafbeelding', $vars['objects']->ID ); ?>');" <?php include( dirname(__FILE__) . '/partials/carousel-colors.php' ); ?> data-id="<?php print_r( $vars['id'] ); ?>">
+<a class="item story nieuwwerk header carousel post-<?php print( $vars['objects']->ID ); ?>" style="background-image: url('<?php the_field( 'achtergrondafbeelding', $vars['objects']->ID ); ?>');" <?php include( dirname(__FILE__) . '/partials/carousel-colors.php' ); ?> data-id="<?php print_r( $vars['id'] ); ?>">
   <?php include( dirname(__FILE__) . '/partials/sponsor.php' ); ?>
-	<h2><?php print( $vars['objects']->post_title ); ?></h2>
-	<h3><?php print( $vars['objects']->post_excerpt ); ?></h3>
+  <div class="carousel-wrapper">
+  	<h2><?php print( $vars['objects']->post_title ); ?></h2>
+  	<h3><?php print( $vars['objects']->post_excerpt ); ?></h3>
+  </div>
 </a>
 
 <?php include( dirname(__FILE__) . '/partials/gradient-bg.php' ); ?>
--- timeline-event-templates/timeline-event-magazine-weekvan-carousel.php
+++ timeline-event-templates/timeline-event-magazine-weekvan-carousel.php
@@ -1,5 +1,4 @@
 <a href="<?php print( get_permalink( $vars['objects']->ID ) ); ?>" class="item story weekvan header carousel" data-second-background="#300747" data-id="<?php print_r( $vars['id'] ); ?>">
-  <?php /* <img src="<?php echo $portrait[0] ?>" class="portrait"> */ ?>
   <?php if( is_null( $vars['sponsor'] ) ): ?>
     <figure class="sponsor">
       <figcaption>presented by</figcaption>
@@ -8,8 +7,11 @@
   <?php else: ?>
     <?php include( dirname(__FILE__) . '/partials/sponsor.php' ); ?>
   <?php endif; ?>
-  <img src="<?php bloginfo('template_directory') ?>/img/logo-weekvan.png" class="logo" />
-  <h2><?php print( $vars['objects']->post_title ); ?></h2>
-  <h3>Chief Pencil bij Present Plus</h3>
-  <h4><?php print( $vars['objects']->post_excerpt ); ?></h4>
+    
+  <div class="carousel-wrapper">
+    <?php $portrait = wp_get_attachment_image_src( get_field( 'portretfoto', $vars['objects']->ID ), 'medium' ); ?>
+    <img src="<?php echo $portrait[0] ?>" class="portrait">
+    <img src="<?php bloginfo('template_directory') ?>/img/logo-weekvan.png" class="logo" />
+    <h3><?php print( $vars['objects']->post_excerpt ); ?></h3>
+  </div>
 </a>
