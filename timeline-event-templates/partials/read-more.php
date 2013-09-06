2261c87e157b442c52c5e19928ed0dd86d94fd32
Erik <erik@fontanel.nl>
2013-09-06 16:37:26 +0200

    better html for read more

--- timeline-event-templates/partials/read-more.php
+++ timeline-event-templates/partials/read-more.php
@@ -1,3 +1,3 @@
 <?php if( isset( $article_parts[1] ) ): ?>
-  &raquo; <a href="<?php print( get_permalink( get_page_by_title( 'notes' ) ) ); ?>?u=<?php print($vars['objects'][0]->pretty_url); ?>">Lees meer</a>
+  <p>&raquo; <a href="<?php print( get_permalink( get_page_by_title( 'notes' ) ) ); ?>?u=<?php print($vars['objects'][0]->pretty_url); ?>">Lees meer</a></p>
 <?php endif; ?>
