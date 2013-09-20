4b20d310295d464e40fffc202f3546bf79c78141
Jasper Kennis <info@jasperkennis.nl>
2013-09-20 17:17:17 +0200

    Readability improved and only fetch published posts.

--- crawlers/fontanel-universal-crawler-timeline-magazine-crawler.class.php
+++ crawlers/fontanel-universal-crawler-timeline-magazine-crawler.class.php
@@ -5,7 +5,10 @@
 			
 			public function fetchPosts( $range ) {
 				global $wp_query;
-				$args = array( 'post_type' => array( 'weekvan', 'nieuwwerk', 'goedbezig', 'fontanel' ), 'posts_per_page' => ( $range == "max" ? 1000 : 10 ) );
+				$args = array(
+				  'post_type' => array( 'weekvan', 'nieuwwerk', 'goedbezig', 'fontanel' ),
+				  'posts_per_page' => ( $range == "max" ? 1000 : 10 ),
+				  'post_status' => 'publish' );
 				query_posts( $args );
 
 				while ( have_posts() ) : the_post();
