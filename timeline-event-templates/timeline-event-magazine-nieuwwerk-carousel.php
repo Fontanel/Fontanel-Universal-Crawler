d0ab19ec620c438f1bcc1b157b01a920dc1ed872
Jasper Kennis <info@jasperkennis.nl>
2013-09-21 15:20:33 +0200

    Using floating logo.

--- timeline-event-templates/timeline-event-magazine-nieuwwerk-carousel.php
+++ timeline-event-templates/timeline-event-magazine-nieuwwerk-carousel.php
@@ -1,4 +1,4 @@
-<div class="rsContent item story nieuwwerk label header carousel post-<?php print( $vars['objects']->ID ); ?>" style="background-image: url('<?php the_field( 'achtergrondafbeelding', $vars['objects']->ID ); ?>');" <?php include( dirname(__FILE__) . '/partials/carousel-colors.php' ); ?> data-id="<?php print_r( $vars['id'] ); ?>">
+<div class="rsContent item story nieuwwerk label floating header carousel post-<?php print( $vars['objects']->ID ); ?>" style="background-image: url('<?php the_field( 'achtergrondafbeelding', $vars['objects']->ID ); ?>');" <?php include( dirname(__FILE__) . '/partials/carousel-colors.php' ); ?> data-id="<?php print_r( $vars['id'] ); ?>">
   <?php include( dirname(__FILE__) . '/partials/sponsor.php' ); ?>
   <div class="carousel-wrapper">
   	<h2><?php print( $vars['objects']->post_title ); ?></h2>
