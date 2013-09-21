28ffbf7947c219a19fd4ae748aac32d94da6fa72
Jasper Kennis <info@jasperkennis.nl>
2013-09-21 14:45:44 +0200

    No longer make the title a link.

--- timeline-event-templates/timeline-event-magazine-nieuwwerk-carousel.php
+++ timeline-event-templates/timeline-event-magazine-nieuwwerk-carousel.php
@@ -1,7 +1,7 @@
 <div class="rsContent item story nieuwwerk label header carousel post-<?php print( $vars['objects']->ID ); ?>" style="background-image: url('<?php the_field( 'achtergrondafbeelding', $vars['objects']->ID ); ?>');" <?php include( dirname(__FILE__) . '/partials/carousel-colors.php' ); ?> data-id="<?php print_r( $vars['id'] ); ?>">
   <?php include( dirname(__FILE__) . '/partials/sponsor.php' ); ?>
   <div class="carousel-wrapper">
-  	<h2><a href="<?php print( get_permalink( $vars['objects']->ID ) ); ?>"><?php print( $vars['objects']->post_title ); ?></a></h2>
+  	<h2><?php print( $vars['objects']->post_title ); ?></h2>
   	<h3><?php print( $vars['objects']->post_excerpt ); ?></h3>
 		<a href="<?php print( get_permalink( $vars['objects']->ID ) ); ?>" class="deep">Lees verder</a>
   </div>
