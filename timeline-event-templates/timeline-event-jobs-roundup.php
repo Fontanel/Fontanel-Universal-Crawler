241d9e8efc38974560065863a99b5717cd2d4ead
Jasper Kennis <info@jasperkennis.nl>
2013-09-26 14:44:40 +0200

    Much smarter url formatting for the weeklies.

--- timeline-event-templates/timeline-event-jobs-roundup.php
+++ timeline-event-templates/timeline-event-jobs-roundup.php
@@ -34,9 +34,10 @@
     	</ul>
 	  </section>
     <footer class="timeline-footer">
-			<div class="fb-share icon-facebook-1" data-url="<?php echo $permalink; ?>" data-title="Like"></div>
-			<div class="tw-share icon-twitter" data-url="<?php echo $permalink; ?>" data-title="Tweet"></div>
-			<time class="icon-clock"><a href="<?php echo $permalink; ?>">Week <?php print( $vars['created_at'] ); ?></a></time>
+      <?php $permalink = site_url( '/weekly/' . Date( 'Y', $vars['created_at'] ) . '/' . Date( 'W', $vars['created_at'] ) ); ?>
+			<div class="fb-share icon-facebook-1" data-url="<?php print( $permalink ); ?>" data-title="Like"></div>
+			<div class="tw-share icon-twitter" data-url="<?php print( $permalink ); ?>" data-title="Tweet"></div>
+			<time class="icon-clock"><a href="<?php print( $permalink ); ?>">Week <?php print( Date( 'W', $vars['created_at'] ) ); ?></a></time>
     </footer>
   </div>
 </article>
