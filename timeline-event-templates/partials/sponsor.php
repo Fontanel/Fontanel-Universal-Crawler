a379ae8521acad224ade227f4907a1d3db2b7cf2
Jasper Kennis <info@jasperkennis.nl>
2013-09-27 13:47:51 +0200

    Styling classes.

--- timeline-event-templates/partials/sponsor.php
+++ timeline-event-templates/partials/sponsor.php
@@ -1,6 +1,6 @@
 <?php if( !is_null( $vars['sponsor'] ) ): ?>
   <figure class="sponsor">
-    <figcaption>presented by</figcaption>
+    <figcaption class="proxima-light">presented by</figcaption>
     <a href="http://www.vitra.nl/" target="_blank"><img src="<?php print( $vars['sponsor']->thumb ); ?>"></a>
   </figure>
 <?php endif; ?>
--- timeline-event-templates/timeline-event-magazine-weekvan-archive.php
+++ timeline-event-templates/timeline-event-magazine-weekvan-archive.php
@@ -1,6 +1,6 @@
 <div class="item story weekvan header archive">
   <figure class="sponsor">
-    <figcaption>presented by</figcaption>
+    <figcaption class="proxima-light">presented by</figcaption>
     <a href="http://www.vitra.nl/" target="_blank"><img src="<?php bloginfo('template_directory') ?>/img/sponsors/vitra-logo-purple.png"></a>
   </figure>
 </div>
